d370f39c4189fea30f18a278db29a74390d46b50
bank-github <6431501089@lamduan.mfu.ac.th>
2024-06-19 14:23:58 +0700

    update cors in main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,22 @@ from fastapi import FastAPI
 from app.routes import manageTags
 from app.routes import manageUser
 from datetime import datetime
+from starlette.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
+
 
 # app.include_router(setting_message.router, prefix='/settings', tags=['settings']) #can use
 app.include_router(manageTags.router, prefix='/tags', tags=['tags'])
