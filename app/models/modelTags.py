b601243d9609e482426f8d17e09eac3e5e04b9fa
bank-github <6431501089@lamduan.mfu.ac.th>
2024-06-21 17:29:09 +0700

    update modelTag

--- app/models/modelTags.py
+++ app/models/modelTags.py
@@ -5,10 +5,15 @@ from typing import Optional
 class Tags(BaseModel):
      tagMac: str
      assetName: str
+     description: str
+     deviceClass: str
 
 class CreateTags(Tags):
      pass
 
 class UpdateTags(Tags):
      tagMac: Optional[str] = None
+     assetName: Optional[str] = None
+     description: Optional[str] = None
+     deviceClass: Optional[str] = None
      
